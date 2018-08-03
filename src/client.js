1ed008972773553e8a713ea3f28f1b787a74e58c
wawhal <rishichandra.wawhal@gmail.com>
2018-08-03 16:30:24 +0530

    adding a GQClient

---
+++ src/client.js
@@ -0,0 +1,23 @@
+const query = require('./query.js');
+
+class GQClient {
+  constructor(args) {
+    this.endpoint = args.endpoint;
+    this.headers = args.headers;
+  }
+
+  query(queryString, variables, name) {
+    return new Promise((resolve, reject) => {
+      query({
+        query: queryString,
+        endpoint: this.endpoint,
+        headers: this.headers,
+        variables,
+        name
+      }).then((response) => resolve(response))
+        .catch((error) => reject(error));
+    });
+  }
+}
+
+module.exports = GQClient;
