bf54f7d79f6e8bf35e48013f355ad3168b252b4c
Alex Kalderimis <akalderimis@gitlab.com>
2021-02-02 18:04:14 +0000

    Add persistent history support

--- src/ui.js
+++ src/ui.js
@@ -1,4 +1,8 @@
 const tk = require('terminal-kit');
+const xdg = require('@folder/xdg');
+const path = require('path');
+const fs = require('fs');
+const {promisify} = require('util');
 const {getIntrospectionQuery, buildClientSchema, parse} = require('graphql');
 const {cli} = require('cli-ux');
 const {validateQuery, getAutocompleteSuggestions} = require('graphql-language-service-interface');
@@ -11,25 +15,25 @@ const query = require('./query.js');
 var term = tk.terminal;
 
 let ib;
+let cancel;
 let schema;
 let exit = false;
 
-const terminate = error => {
+const directories = xdg({subdir: 'graphqurl'});
+
+const terminate = () => {
+  if (ib) {
+    ib.abort();
+  }
+  if (cancel) {
+    cancel();
+  }
   term.nextLine(1);
   term.grabInput(false);
   term.fullscreen(false);
 };
 
-const input = (history, value) => {
-  if (ib) {
-    ib.abort();
-  }
-  let opts = { default: value, ...inputOpts(history) };
-  ib = term.inputField(opts);
-  return ib;
-}
-
-const suggestField = (inputString) => {
+const suggestField = inputString => {
   let position = ib.getCursorPosition();
   let bStack = [];
   let qs = inputString.slice(0, position + 1);
@@ -61,38 +65,33 @@ const suggestField = (inputString) => {
     items.unshift(bStackACS[bStackACS.length - 1]);
   }
 
-  if (items.length == 0) { 
+  if (items.length === 0) {
     return inputString;
   }
 
   items.prefix = inputString.replace(/\w+$/, '');
 
   return items;
-}
+};
 
-const inputOpts = (history) => {
-  return {
-    history: history,
-    autoComplete: suggestField,
-    autoCompleteMenu: true
-  };
+const autocompletion = {autoComplete: suggestField, autoCompleteMenu: true};
+
+const input = (history, value) => {
+  if (ib) {
+    ib.abort();
+  }
+  let opts = {default: value, history, ...autocompletion};
+  ib = term.inputField(opts);
+  return ib;
 };
 
 term.on('key', async function (key) {
-  if (key === 'CTRL_C') {
-    terminate('cancelled');
-  }
-  if (key === 'CTRL_D') {
+  if (key === 'CTRL_D' || key === 'CTRL_C') {
+    exit = true;
     console.log('Goodbye!');
-    terminate('cancelled');
+    terminate();
   }
 });
-
-const getQueryFromTerminalUI = (history, value)  => {
-  return getValidQuery(history, value).catch(invalidStr => {
-    return getQueryFromTerminalUI(history, invalidStr)
-  });
-};
 
 const getValidQuery = async (history, value) => {
   term('gql> ');
@@ -101,11 +100,11 @@ const getValidQuery = async (history, value) => {
 
   try {
     errors = await validateQuery(parse(qs), schema);
-  } catch(e) {
-    errors = [e]
+  } catch (e) {
+    errors = [e];
   }
 
-  if (errors.length > 0) { 
+  if (errors.length > 0) {
     term.nextLine(1);
     term.bold('Invalid query\n');
     errors.forEach(e => {
@@ -115,6 +114,41 @@ const getValidQuery = async (history, value) => {
   }
 
   return qs;
+};
+
+const getQueryFromTerminalUI = (history, value)  => {
+  return getValidQuery(history.slice(), value).catch(invalidStr => {
+    if (exit) return Promise.resolve(undefined);
+
+    return getQueryFromTerminalUI(history, invalidStr);
+  });
+};
+
+const historyFile = path.join(directories.config, 'history');
+
+const writeHistory = history => {
+  if (history.length > 0) {
+    fs.mkdir(directories.config, {recursive: true}, err => {
+      if (err) return;
+
+      fs.writeFile(historyFile, history.join('\n'), 'utf8', err => {
+        if (!err) return;
+        console.warn('Could not write history file');
+      });
+    });
+  }
+};
+
+const loadHistory = async () => {
+  try {
+    let fileContent = await promisify(fs.readFile)(historyFile, 'utf8');
+    return fileContent.split('\n');
+  } catch (e) {
+    if (e.code !== 'ENOENT') {
+      console.warn(e);
+    }
+    return [];
+  }
 };
 
 const executeQueryFromTerminalUI = async (queryOptions, successCb, errorCb)  => {
@@ -133,18 +167,28 @@ const executeQueryFromTerminalUI = async (queryOptions, successCb, errorCb)  => 
   const r = schemaResponse.data;
   // term.fullscreen(true);
   schema = buildClientSchema(r);
-  console.log('Enter the query, use TAB to auto-complete, Ctrl+Q / Enter to execute, Ctrl+C to cancel');
 
   /* eslint-disable-next-line no-unmodified-loop-condition */
-  const history = [];
-  while (!exit) {
-    /* eslint-disable-next-line no-await-in-loop */
-    const queryString = await getQueryFromTerminalUI(history.slice());
-    history.push(queryString);
-    cli.action.start('Waiting');
-    await query({query: queryString, endpoint, headers}, successCb, errorCb);
-    cli.action.stop('done');
-  }
+  let history = await loadHistory();
+
+  console.log('Enter the query, use TAB to auto-complete, Enter to execute, Ctrl+C to cancel');
+  let cancellation = new Promise((resolve, _) => {
+    cancel = () => resolve(undefined);
+  });
+
+  let queryString;
+
+  do {
+    queryString = await Promise.race([getQueryFromTerminalUI(history), cancellation]);
+    if (queryString) {
+      history.push(queryString);
+      cli.action.start('Waiting');
+      await query({query: queryString, endpoint, headers}, successCb, errorCb);
+      cli.action.stop('done');
+    }
+  } while (queryString);
+
+  writeHistory(history);
 };
 
 module.exports = executeQueryFromTerminalUI;
